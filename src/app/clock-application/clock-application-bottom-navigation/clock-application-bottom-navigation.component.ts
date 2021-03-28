d508a93c42bd36ae132a4dc3b02d85768b447963
arturgawlik <artur.gawlik@icloud.com>
2021-03-28 22:05:20 +0200

    feat: mobile view

--- src/app/clock-application/clock-application-bottom-navigation/clock-application-bottom-navigation.component.ts
+++ src/app/clock-application/clock-application-bottom-navigation/clock-application-bottom-navigation.component.ts
@@ -16,30 +16,30 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <div class="h-14 backgroundColor flex">
         <nav class="flex items-end w-full">
-          <div class="mx-auto flex flex-col items-center">
+          <a class="mx-auto flex flex-col items-center cursor-pointer">
             <app-svg svgPath="/assets/svgs/earth.svg" containerClass="" svgClass="h-7"></app-svg>
             <span class="fontSize color mt-1">
               Zegary Świata
             </span>
-          </div>
-          <div class="mx-auto flex flex-col items-center">
+          </a>
+          <a class="mx-auto flex flex-col items-center cursor-pointer">
             <app-svg svgPath="/assets/svgs/clock.svg" containerClass="" svgClass="h-7"></app-svg>
             <span class="fontSize color mt-1">
               Budzik
             </span>
-          </div>
-          <div class="mx-auto flex flex-col items-center">
+          </a>
+          <a class="mx-auto flex flex-col items-center cursor-pointer">
             <app-svg svgPath="/assets/svgs/stopwatch.svg" containerClass="" svgClass="h-7 fill-current text-yellow-600"></app-svg>
             <span class="fontSize mt-1 text-yellow-600">
               Stoper
             </span>
-          </div>
-          <div class="mx-auto flex flex-col items-center">
+          </a>
+          <a class="mx-auto flex flex-col items-center cursor-pointer">
             <app-svg svgPath="/assets/svgs/countdownTimer.svg" containerClass="" svgClass="h-7"></app-svg>
             <span class="fontSize color mt-1">
               Minutnik
             </span>
-          </div>
+          </a>
         </nav>
     </div>
   `
--- src/app/clock-application/clock-application.component.ts
+++ src/app/clock-application/clock-application.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-clock-application',
   styles: [`
-    .wrapper {
-      width: 320px;
-      height: 568px;
+    @media (min-width: 640px) {
+      .wrapper {
+        width: 320px;
+        height: 568px;
+      }
     }
   `],
   template: `
-    <div class="bg-black wrapper border relative">
+    <div class="bg-black wrapper border relative w-screen h-screen">
       <router-outlet></router-outlet>
       <div class="absolute inset-x-0 bottom-0">
         <app-clock-application-bottom-navigation></app-clock-application-bottom-navigation>
