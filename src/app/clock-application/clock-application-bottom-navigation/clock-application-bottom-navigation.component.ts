1574cbe541459087a5f37fbd1cdf55cc9b823771
arturgawlik <artur.gawlik@icloud.com>
2021-03-28 22:50:56 +0200

    feat: add links

--- src/app/clock-application/clock-application-bottom-navigation/clock-application-bottom-navigation.component.ts
+++ src/app/clock-application/clock-application-bottom-navigation/clock-application-bottom-navigation.component.ts
@@ -16,25 +16,25 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <div class="h-14 backgroundColor flex">
         <nav class="flex items-end w-full">
-          <a class="mx-auto flex flex-col items-center cursor-pointer">
+          <a routerLink="/world-clocks" class="mx-auto flex flex-col items-center cursor-pointer">
             <app-svg svgPath="/assets/svgs/earth.svg" containerClass="" svgClass="h-7"></app-svg>
             <span class="fontSize color mt-1">
               Zegary Świata
             </span>
           </a>
-          <a class="mx-auto flex flex-col items-center cursor-pointer">
+          <a routerLink="/alarm" class="mx-auto flex flex-col items-center cursor-pointer">
             <app-svg svgPath="/assets/svgs/clock.svg" containerClass="" svgClass="h-7"></app-svg>
             <span class="fontSize color mt-1">
               Budzik
             </span>
           </a>
-          <a class="mx-auto flex flex-col items-center cursor-pointer">
+          <a routerLink="/stopwatch" class="mx-auto flex flex-col items-center cursor-pointer">
             <app-svg svgPath="/assets/svgs/stopwatch.svg" containerClass="" svgClass="h-7 fill-current text-yellow-600"></app-svg>
             <span class="fontSize mt-1 text-yellow-600">
               Stoper
             </span>
           </a>
-          <a class="mx-auto flex flex-col items-center cursor-pointer">
+          <a routerLink="/countdown-timer" class="mx-auto flex flex-col items-center cursor-pointer">
             <app-svg svgPath="/assets/svgs/countdownTimer.svg" containerClass="" svgClass="h-7"></app-svg>
             <span class="fontSize color mt-1">
               Minutnik
